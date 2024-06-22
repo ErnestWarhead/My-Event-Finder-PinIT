56e85d2c732fc8307d75d0b38a2482d2f3628f83
Ernesto Martinez <61639551+ErnestWarhead@users.noreply.github.com>
2024-06-22 20:23:48 +0000

    changing index path

--- src/app.py
+++ src/app.py
@@ -57,7 +57,7 @@ def handle_invalid_usage(error):
 def sitemap():
     if ENV == "development":
         return generate_sitemap(app)
-    return send_from_directory(static_file_dir, 'template.html')
+    return send_from_directory(static_file_dir, 'index.html')
 
 
 @app.route('/routes', methods=['GET'])
@@ -76,7 +76,7 @@ def list_routes():
 @app.route('/<path:path>', methods=['GET'])
 def serve_any_other_file(path):
     if not os.path.isfile(os.path.join(static_file_dir, path)):
-        path = 'template.html'
+        path = 'index.html'
     response = send_from_directory(static_file_dir, path)
     response.cache_control.max_age = 0  # avoid cache memory
     return response
