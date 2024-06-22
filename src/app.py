c836c877ec83c179960790783b2e20b5f0ad4b15
Ernesto Martinez <61639551+ErnestWarhead@users.noreply.github.com>
2024-06-22 18:46:42 +0000

    Preparing for Heroku

--- src/app.py
+++ src/app.py
@@ -18,7 +18,7 @@ from api.commands import setup_commands
 # Determine the environment
 ENV = "development" if os.getenv("FLASK_DEBUG") == "1" else "production"
 static_file_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../public/')
-app = Flask(__name__)
+app = Flask(__name__, static_folder='build')
 app.url_map.strict_slashes = False 
 
 # Database configuration
